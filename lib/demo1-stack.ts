ef60aac43bd31b08812882db25cdcbe8c9cd3b1e
Chris Johnson <chris.johnson@secureframe.com>
2023-05-13 10:36:49 -0500

    working demo

--- lib/demo1-stack.ts
+++ lib/demo1-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+import { Duration } from 'aws-cdk-lib';
 
 export class Demo1Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'MyBucket', {
+      versioned: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Demo1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaFn = new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.RUBY_2_7,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+      timeout: Duration.seconds(10),
+    });
+
+    bucket.grantWrite(lambdaFn);
+
+    new cdk.CfnOutput(this, 'BucketName', { value: bucket.bucketName });
+    new cdk.CfnOutput(this, 'LambdaFunctionName', { value: lambdaFn.functionName });
   }
 }
---
+++ lib/lambda/index.rb
@@ -0,0 +1,15 @@
+require 'aws-sdk'
+
+def handler(event:, context:)
+  bucket = ENV['BUCKET_NAME']
+  # Create a timestamp file name
+  timestamp = Time.now.utc.strftime('%Y%m%dT%H%M%S%z')
+  file_name = "timestamp-#{timestamp}.txt"
+
+  # Write the timestamp file to S3
+  s3 = Aws::S3::Client.new
+  s3.put_object(bucket: bucket, key: file_name, body: timestamp)
+
+  # Return a success response
+  { statusCode: 200, body: 'Timestamp file written to S3' }
+end
